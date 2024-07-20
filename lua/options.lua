e440297d72a404f7d54e08ad7d75743fd255ca67
Marcos Romero Lamas <marromlam@gmail.com>
2024-07-20 23:24:16 +0200

    feat: add options

---
+++ lua/options.lua
@@ -0,0 +1,210 @@
+-- Message output on vim actions {{{
+vim.opt.shortmess = {
+  t = true, -- truncate file messages at start
+  A = true, -- ignore annoying swap file messages
+  o = true, -- file-read message overwrites previous
+  O = true, -- file-read message overwrites previous
+  T = true, -- truncate non-file messages in middle
+  f = true, -- (file x of x) instead of just (x of x
+  F = true, -- Don't give file info when editing, NOTE: this breaks autocommand messages
+  s = true,
+  c = true,
+  W = true, -- Don't show [w] or written when writing
+}
+-- }}}
+
+-- Timings {{{
+vim.opt.updatetime = 200
+vim.opt.timeout = true
+vim.opt.timeoutlen = 200
+-- }}}
+
+-- Window splitting and buffers {{{
+
+vim.opt.splitkeep = 'screen'
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.eadirection = 'hor'
+-- exclude usetab pde we do not want to jump to buffers in already open tabs
+-- do not use split or vsplit to ensure we don't open any new windows
+vim.opt.switchbuf = 'useopen,uselast'
+vim.opt.fillchars = {
+  eob = ' ', -- suppress ~ at EndOfBuffer
+  diff = '╱', -- alternatives = ⣿ ░ ─
+  msgsep = ' ', -- alternatives: ‾ ─
+  fold = ' ',
+  foldopen = '▽', -- '▼'
+  foldclose = '▷', -- '▶'
+  foldsep = ' ',
+}
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+-- }}}
+
+-- Diff {{{
+-- Use in vertical diff mode, blank lines to keep sides aligned, Ignore
+-- whitespace changes
+vim.opt.diffopt = vim.opt.diffopt
+  + {
+    'vertical',
+    'iwhite',
+    'hiddenoff',
+    'foldcolumn:0',
+    'context:4',
+    'algorithm:histogram',
+    'indent-heuristic',
+    'linematch:60',
+  }
+-- }}}
+
+-- Format Options {{{
+vim.opt.formatoptions = {
+  ['1'] = true,
+  ['2'] = true, -- Use indent from 2nd line of a paragraph
+  q = true, -- continue comments with gq"
+  c = true, -- Auto-wrap comments using textwidth
+  r = true, -- Continue comments when pressing Enter
+  n = true, -- Recognize numbered lists
+  t = false, -- autowrap lines using text width value
+  j = true, -- remove a comment leader when joining lines.
+  -- Only break if the line was not longer than 'textwidth' when the insert
+  -- started and only at a white character that has been entered during the
+  -- current insert command.
+  l = true,
+  v = true,
+}
+
+-- Folds {{{
+-- unfortunately folding in (n)vim is a mess, if you set the fold level to start
+-- at X then it will auto fold anything at that level, all good so far. If you then
+-- try to edit the content of your fold and the foldmethod=manual then it will
+-- recompute the fold which when using nvim-ufo means it will be closed again...
+
+vim.opt.foldlevelstart = 999
+
+-- }}}
+
+-- Grepprg
+vim.opt.grepprg = [[rg --glob "!.git" --no-heading --vimgrep --follow $*]]
+vim.opt.grepformat = vim.opt.grepformat ^ { '%f:%l:%c:%m' }
+
+-- Display
+vim.opt.conceallevel = 2
+vim.opt.breakindent = true
+vim.opt.breakindentopt = 'sbr'
+vim.opt.linebreak = true -- lines wrap at words rather than random characters
+-- vim.opt.signcolumn = "yes:1"
+vim.opt.signcolumn = 'yes'
+-- vim.opt.signcolumn = "no"
+vim.opt.ruler = false
+vim.opt.cmdheight = 0
+vim.opt.showbreak = [[↪ ]] -- Options include -> '…', '↳ ', '→','↪ '
+
+-- List chars
+vim.opt.list = true -- invisible chars
+vim.opt.listchars = {
+  eol = nil,
+  extends = '…', -- Alternatives: … » ›
+  precedes = '░', -- Alternatives: … « ‹
+  tab = '» ',
+  trail = '·',
+  nbsp = '␣',
+}
+
+-- Indentation
+vim.opt.wrap = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.wrapmargin = 4
+vim.opt.textwidth = 80
+vim.opt.autoindent = true
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.opt.pumheight = 15
+vim.opt.confirm = true -- make vim prompt me to save before doing destructive things
+vim.opt.completeopt = { 'menuone', 'noselect' }
+vim.opt.hlsearch = true
+vim.opt.autowriteall = true -- automatically :write before running commands and changing files
+vim.opt.clipboard = { 'unnamedplus' }
+vim.o.laststatus = 3
+vim.o.termguicolors = true
+vim.o.guifont = 'CartographCF Nerd Font Mono:h14,codicon'
+vim.opt.inccommand = 'split'
+vim.g.have_nerd_font = true
+
+-- Emoji
+vim.opt.emoji = false
+
+-- Cursor
+vim.opt.guicursor = {
+  'n-v-c-sm:block-Cursor',
+  'i-ci-ve:ver25-iCursor',
+  'r-cr-o:hor20-Cursor',
+  'a:blinkon0',
+}
+vim.opt.cursorlineopt = { 'both' }
+-- vim.opt.cursorline = true
+
+-- Title
+vim.opt.titleold = vim.fn.fnamemodify(vim.loop.os_getenv('SHELL'), ':t')
+vim.opt.title = true
+vim.opt.titlelen = 70
+
+-- Utilities
+vim.opt.showmode = false
+vim.opt.sessionoptions = {
+  'globals',
+  'buffers',
+  'curdir',
+  'winpos',
+  'winsize',
+  'help',
+  'tabpages',
+  'terminal',
+}
+vim.opt.viewoptions = { 'cursor', 'folds' } -- save/restore just these (with `:{mk,load}view`)
+vim.opt.virtualedit = 'block' -- allow cursor to move where there is no text in visual block mode
+
+-- Jumplist
+vim.opt.jumpoptions = { 'stack' } -- make the jumplist behave like a browser stack
+
+-- Backup and swaps
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
+vim.opt.swapfile = false
+
+-- Match and search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wrapscan = true -- Searches wrap around the end of the file
+vim.opt.scrolloff = 9
+vim.opt.sidescrolloff = 10
+vim.opt.sidescroll = 1
+-----------------------------------------------------------------------------//
+-- Spelling {{{1
+-----------------------------------------------------------------------------//
+vim.opt.spellsuggest:prepend({ 12 })
+vim.opt.spelloptions:append({ 'camel', 'noplainbuffer' })
+vim.opt.spellcapcheck = '' -- don't check for capital letters at start of sentence
+-----------------------------------------------------------------------------//
+-- Mouse {{{1
+-----------------------------------------------------------------------------//
+vim.opt.mouse = 'a'
+vim.opt.mousefocus = true
+vim.opt.mousemoveevent = true
+vim.opt.mousescroll = { 'ver:1', 'hor:6' }
+-----------------------------------------------------------------------------//
+-- Allow project local vimrc files example, .nvim.lua or .nvimrc see :h exrc
+-- netrw {{{
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+-- }}}
+--
+vim.opt.isfname:append('@-@')
+
+-- vim: fdm=marker
