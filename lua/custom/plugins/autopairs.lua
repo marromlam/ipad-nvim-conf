a27778c968f88f04f53b40c64811aaeba6a30ae7
Marcos Romero Lamas <marromlam@gmail.com>
2024-07-26 00:14:59 +0200

    feat: add autopais

---
+++ lua/custom/plugins/autopairs.lua
@@ -0,0 +1,31 @@
+if vim.g.is_ipad then
+  return {}
+end
+
+
+return {
+  'windwp/nvim-autopairs',
+  event = { 'InsertEnter' },
+  dependencies = {
+    'hrsh7th/nvim-cmp',
+  },
+  config = function()
+    -- import nvim-autopairs
+    local autopairs = require 'nvim-autopairs'
+
+    -- configure autopairs
+    autopairs.setup {
+      check_ts = true, -- enable treesitter
+      ts_config = {
+        lua = { 'string' }, -- don't add pairs in lua string treesitter nodes
+        javascript = { 'template_string' }, -- don't add pairs in javscript template_string treesitter nodes
+        java = false, -- don't check treesitter on java
+      },
+    }
+
+    -- import nvim-autopairs completion functionality
+    local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+    local cmp = require 'cmp'
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+  end,
+}
